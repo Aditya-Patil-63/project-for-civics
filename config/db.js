175c7b516ba19e692911c566549325f7aaff3b4c
Aditya <abpatil.ac@gmail.com>
2026-08-01 20:01:03 +0530

    Fix Aiven MySQL SSL connection

--- config/db.js
+++ config/db.js
@@ -20,9 +20,15 @@ const connectMongoDB = async () => {
 // MySQL Connection Pool
 const mysqlPool = mysql.createPool({
     host: process.env.MYSQL_HOST,
+    port: Number(process.env.MYSQL_PORT),
     user: process.env.MYSQL_USER,
     password: process.env.MYSQL_PASSWORD,
     database: process.env.MYSQL_DATABASE,
+
+    ssl: {
+        rejectUnauthorized: false
+    },
+
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
